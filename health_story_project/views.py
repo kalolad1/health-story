71a6095ac01651895e2b42db1286a971f735cd73
Darshan Kalola <darshanvkalola@gmail.com>
2018-12-31 15:25:13 -0500

    Finish left panel navigation bar

--- health_story_project/views.py
+++ health_story_project/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def landing_page(request):
+    """Displays the landing page, unless the user is logged in.
+
+    If the user is logged in, redirect them to the homepage instead of
+    the landing page.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    if request.user.is_authenticated:
+        return redirect('health_story/home')
+
     return render(request, '../templates/landing-page.html')
